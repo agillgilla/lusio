a5928ee2615f90202da5e7f7480c3381d71257f2
Arjun Gill <agillgilla@gmail.com>
2021-11-20 09:47:35 -0800

    Fix sudo commands in setup script

--- setup.sh
+++ setup.sh
@@ -13,9 +13,9 @@ Exec=sudo /usr/bin/python3 /home/pi/lusio/lusio.py
 ' >> /home/pi/.config/autostart/lusio.desktop
 
 # Disable screen blanking
-mkdir -p /etc/X11/xorg.conf.d/
-cp /usr/share/raspi-config/10-blanking.conf /etc/X11/xorg.conf.d/
-printf "\\033[9;0]" >> /etc/issue
+sudo mkdir -p /etc/X11/xorg.conf.d/
+sudo cp /usr/share/raspi-config/10-blanking.conf /etc/X11/xorg.conf.d/
+sudo sh -c "printf '\\033[9;0]' >> /etc/issue"
 
 # Install VLC
 sudo apt update
@@ -27,7 +27,6 @@ cd /home/pi/lusio
 pip install -r requirements.txt
 
 # Disable HDMI-CEC
-sudo su
-sudo echo '
+sudo sh -c "echo '
 hdmi_ignore_cec_init=1
-hdmi_ignore_cec=1' >> /boot/config.txt
+hdmi_ignore_cec=1' >> /boot/config.txt"
